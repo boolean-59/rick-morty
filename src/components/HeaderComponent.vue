ce855236eddd54d15335d0eef14e66aadbfdacf3
klelia <clelia.fradella@gmail.com>
2022-05-06 08:20:23 +0200

    remove component header

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="container">
+        <h1 class="text-center my-5">{{ message }}</h1>
+    </header>
+</template>
+
+<script>
+
+export default {
+  name: 'HeaderComponent',
+  props: {
+    message: String
+  }
+}
+</script>
+
+
+<style scoped lang="scss">
+h1 {
+    font-weight: 700;
+}
+
+</style>
